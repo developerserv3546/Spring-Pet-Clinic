c8485314007234ad5466c1ecafbdb1db826eb470
Serega <serv3546@gmail.com>
2018-10-24 15:33:42 -0700

    adding base entity. Closes #10

--- pet-clinic-data/src/main/java/com/serega/petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/serega/petclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.serega.petclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/com/serega/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/serega/petclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.serega.petclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
 
     private String name;
 
