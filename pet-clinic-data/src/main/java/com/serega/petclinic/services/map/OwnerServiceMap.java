bbc29a6c0fa1675dd7e6208f29c4ccb9ccd8b200
Serega <serv3546@gmail.com>
2018-10-24 20:10:23 -0700

    Impl Map based Services, closes #6

---
+++ pet-clinic-data/src/main/java/com/serega/petclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,34 @@
+package com.serega.petclinic.services.map;
+
+import com.serega.petclinic.model.Owner;
+import com.serega.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+}
---
+++ pet-clinic-data/src/main/java/com/serega/petclinic/services/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package com.serega.petclinic.services.map;
+
+import com.serega.petclinic.model.Pet;
+import com.serega.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+}
