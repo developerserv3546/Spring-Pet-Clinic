f618e844051d19f1b0a8239b4c0d71b43a848d94
Serega <serv3546@gmail.com>
2018-10-29 10:51:29 -0700

    Added SpecialityMapService and SpecialitiesService. Closes #31

---
+++ pet-clinic-data/src/main/java/com/serega/petclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package com.serega.petclinic.services;
+
+import com.serega.petclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/serega/petclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,36 @@
+package com.serega.petclinic.services.map;
+
+import com.serega.petclinic.model.Speciality;
+import com.serega.petclinic.services.SpecialitiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialitiesService {
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
