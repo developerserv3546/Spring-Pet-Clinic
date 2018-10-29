c9daee4fcef2ddb3feab4130b4ed42f2b5c7b0f0
Serega <serv3546@gmail.com>
2018-10-29 01:58:18 -0700

    Added Pets and Contact Info to Owners on startup. Closes #33

--- pet-clinic-data/src/main/java/com/serega/petclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/serega/petclinic/services/map/OwnerServiceMap.java
@@ -1,13 +1,24 @@
 package com.serega.petclinic.services.map;
 
 import com.serega.petclinic.model.Owner;
+import com.serega.petclinic.model.Pet;
 import com.serega.petclinic.services.OwnerService;
+import com.serega.petclinic.services.PetService;
+import com.serega.petclinic.services.PetTypeService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
+
+    private final PetTypeService petTypeService;
+    private final PetService petService;
+
+    public OwnerServiceMap(PetTypeService petTypeService, PetService petService) {
+        this.petTypeService = petTypeService;
+        this.petService = petService;
+    }
 
     @Override
     public Set<Owner> findAll() {
@@ -26,7 +37,29 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
 
     @Override
     public Owner save(Owner object) {
-        return super.save(object);
+
+        if (object != null) {
+            if (object.getPets() != null) {
+                object.getPets().forEach(pet -> {
+                    if (pet.getPetType() != null) {
+                        if (pet.getPetType().getId() == null) {
+                            pet.setPetType(petTypeService.save(pet.getPetType()));
+                        }
+                    } else {
+                        throw new RuntimeException("PetType is required!");
+                    }
+
+                    if (pet.getId() == null) {
+                        Pet savedPet = petService.save(pet);
+                        pet.setId(savedPet.getId());
+                    }
+                });
+            }
+            return super.save(object);
+        } else {
+            return null;
+        }
+
     }
 
     @Override
--- pet-clinic-web/src/main/java/com/serega/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/serega/petclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package com.serega.petclinic.bootstrap;
 
 import com.serega.petclinic.model.Owner;
+import com.serega.petclinic.model.Pet;
 import com.serega.petclinic.model.PetType;
 import com.serega.petclinic.model.Vet;
 import com.serega.petclinic.services.OwnerService;
@@ -8,6 +9,8 @@ import com.serega.petclinic.services.PetTypeService;
 import com.serega.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -36,12 +39,32 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
+        owner1.setAddress("123 Brickerel");
+        owner1.setCity("Miami");
+        owner1.setTelephone("123456789");
+
+        Pet mikesPet = new Pet();
+        mikesPet.setPetType(savedDogPetType);
+        mikesPet.setOwner(owner1);
+        mikesPet.setBirthDate(LocalDate.now());
+        mikesPet.setName("Rasco");
+        owner1.getPets().add(mikesPet);
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
+        owner2.setAddress("123 Brickerel");
+        owner2.setCity("Miami");
+        owner2.setTelephone("123456789");
+
+        Pet fionasCat= new Pet();
+        fionasCat.setName("Just Cat");
+        fionasCat.setOwner(owner2);
+        fionasCat.setBirthDate(LocalDate.now());
+        fionasCat.setPetType(cat);
+        owner2.getPets().add(fionasCat);
 
         ownerService.save(owner2);
 
