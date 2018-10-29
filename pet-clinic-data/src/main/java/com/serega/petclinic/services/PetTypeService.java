54cc4be4e9a8e43b8be3d5066433e4b281f625ad
Serega <serv3546@gmail.com>
2018-10-29 01:16:59 -0700

    Added PetTypeMapService. Closes #29

---
+++ pet-clinic-data/src/main/java/com/serega/petclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.serega.petclinic.services;
+
+import com.serega.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/serega/petclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,36 @@
+package com.serega.petclinic.services.map;
+
+import com.serega.petclinic.model.PetType;
+import com.serega.petclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}
