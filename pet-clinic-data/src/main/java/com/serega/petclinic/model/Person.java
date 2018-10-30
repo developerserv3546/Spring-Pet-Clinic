d1151f5286b55bb8ba6d5eabd836888d06975adf
Serega <serv3546@gmail.com>
2018-10-29 22:48:36 -0700

    Updated Owner with JPA entity mappings. Closes #36

--- pet-clinic-data/src/main/java/com/serega/petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/serega/petclinic/model/Person.java
@@ -1,8 +1,15 @@
 package com.serega.petclinic.model;
 
-public class Person extends BaseEntity{
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
 
+@MappedSuperclass
+public class Person extends BaseEntity {
+
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
 
     public String getFirstName() {
