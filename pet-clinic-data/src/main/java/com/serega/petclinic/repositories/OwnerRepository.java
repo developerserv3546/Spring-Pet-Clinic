09d11a28e9b33e9920bc39143a20154bf0530946
Serega <serv3546@gmail.com>
2018-10-30 02:01:24 -0700

    Created Spring Data JPA Service for Owners. Closes #40

--- pet-clinic-data/src/main/java/com/serega/petclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/serega/petclinic/repositories/OwnerRepository.java
@@ -4,4 +4,6 @@ import com.serega.petclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner,Long> {
+
+    Owner findByLastName(String lastName);
 }
