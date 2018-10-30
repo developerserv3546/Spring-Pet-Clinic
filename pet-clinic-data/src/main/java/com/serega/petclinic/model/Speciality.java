0f91a8814a95f95317954f79f47a7812c8cd7096
Serega <serv3546@gmail.com>
2018-10-29 22:55:54 -0700

    Updated Vet with JPA entity mappings. Closes #37

--- pet-clinic-data/src/main/java/com/serega/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/serega/petclinic/model/Speciality.java
@@ -1,7 +1,14 @@
 package com.serega.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
