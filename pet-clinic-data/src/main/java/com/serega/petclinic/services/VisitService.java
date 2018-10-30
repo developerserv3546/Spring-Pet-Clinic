186942094b7549c5cb7fbca4f47f7582afec3341
Serega <serv3546@gmail.com>
2018-10-30 15:18:42 -0700

    Created Visit Map Service. Closes #46

---
+++ pet-clinic-data/src/main/java/com/serega/petclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.serega.petclinic.services;
+
+import com.serega.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/serega/petclinic/services/map/VisitMapService.java
@@ -0,0 +1,40 @@
+package com.serega.petclinic.services.map;
+
+import com.serega.petclinic.model.Visit;
+import com.serega.petclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if (visit.getPet() == null || visit.getPet().getOwner() == null
+                || visit.getPet().getId() == null || visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid Visit");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
